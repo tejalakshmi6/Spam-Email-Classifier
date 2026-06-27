988ca15c2cb6784c0236d5cc14f9af05433e82f7
Tejalakshmi <tejalakshmi.sethu@gmail.com>
2026-06-27 16:05:12 +0530

    Design frontend & add features

--- app.py
+++ app.py
@@ -18,13 +18,24 @@ def predict():
 
     email_vector = vectorizer.transform([email])
     prediction = model.predict(email_vector)
+    probability = model.predict_proba(email_vector)
+    confidence = max(probability[0]) * 100
+    confidence = round(confidence, 2)
 
     if prediction[0] == 1:
-        result = "Spam 🚫"
+        result = "🚫 Spam"
+        result_class = "spam"
     else:
-        result = "Not Spam ✅"
+        result = "✅ Not Spam"
+        result_class = "ham"
 
-    return result
+    return render_template(
+    "index.html",
+    prediction=result,
+    confidence=confidence,
+    result_class=result_class,
+    email=email
+)
 
 if __name__ == "__main__":
     app.run(debug=True)
